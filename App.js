a6398fe073051a04fd40a4b0b9021cbe552ddc74
htmlDoctor <46823017+htmlDoctor@users.noreply.github.com>
2019-04-30 14:07:31 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+import Title from './Title.js'
+
+function App() {
+  return (
+    <div >
+      <Title name="What we do"/>
+      <Title name="Our work"/>
+      <Title name="Our Team"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Title.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Title(props){
+  return(
+    <div className="Title">
+      <p>{props.name}</p>
+    </div>
+  )
+}
+export default Title
